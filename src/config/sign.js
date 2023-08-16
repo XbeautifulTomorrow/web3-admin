fa1e778be18fa7d8ba29a2f20164e7f24b90962b
Ikaris-a <tyrone00799@gmail.com>
2023-08-16 22:49:14 +0800

    fix: 更新合约

--- src/config/sign.js
+++ src/config/sign.js
@@ -1,4 +1,4 @@
 export default {
-  MultiSign: "0x6311ad21c46e0867028a283118a3e9ddfec1a9af", //MultiSign 合约地址
-  nftHelpAddress: "0x5062162b0b29ceabe92cd679587977650d91066d", //新的nft转账合约地址
+  MultiSign: "0xf5293915f239a8bd6b1fc00c3d5510abd6e97774", //MultiSign 合约地址
+  nftHelpAddress: "0x0636ac626382270f428d23aae75957418a5344fe", //新的nft转账合约地址
 };
