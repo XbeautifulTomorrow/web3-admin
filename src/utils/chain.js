2939e6134cdba2b97b7e4d17fea74a2ffd427efb
sophiayk <857727446@qq.com>
2023-09-05 17:42:24 +0800

    fix:main linj

--- src/utils/chain.js
+++ src/utils/chain.js
@@ -5,100 +5,97 @@ import config from "@/config/env";
 export const chainOptions = {
   test: {
     97: {
-      name: 'BSC',
-      chainId: '97',
-      method: 'wallet_addEthereumChain',
+      name: "BSC",
+      chainId: "97",
+      method: "wallet_addEthereumChain",
       chainParmas: {
-        chainId: '0x61',
-        chainName: 'Binance Smart Chain Testnet',
+        chainId: "0x61",
+        chainName: "Binance Smart Chain Testnet",
         nativeCurrency: {
-          name: 'tBNB',
-          symbol: 'tBNB',
+          name: "tBNB",
+          symbol: "tBNB",
           decimals: 18,
         },
-        rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
-        blockExplorerUrls: ['https://testnet.bscscan.com'],
+        rpcUrls: ["https://data-seed-prebsc-1-s1.binance.org:8545"],
+        blockExplorerUrls: ["https://testnet.bscscan.com"],
       },
     },
     4: {
-      name: 'ETH',
-      chainId: '4',
-      method: 'wallet_switchEthereumChain',
+      name: "ETH",
+      chainId: "4",
+      method: "wallet_switchEthereumChain",
       chainParmas: {
-        chainId: '0x4',
+        chainId: "0x4",
       },
     },
     65: {
-      name: 'OKEX',
-      chainId: '65',
-      method: 'wallet_addEthereumChain',
+      name: "OKEX",
+      chainId: "65",
+      method: "wallet_addEthereumChain",
       chainParmas: {
-        chainId: '0x41',
-        chainName: 'OKExChain Testnet',
+        chainId: "0x41",
+        chainName: "OKExChain Testnet",
         nativeCurrency: {
-          name: 'OKT',
-          symbol: 'OKT',
+          name: "OKT",
+          symbol: "OKT",
           decimals: 18,
         },
-        rpcUrls: ['https://exchaintestrpc.okex.org'],
-        blockExplorerUrls: ['https://www.oklink.com/okexchain-test'],
+        rpcUrls: ["https://exchaintestrpc.okex.org"],
+        blockExplorerUrls: ["https://www.oklink.com/okexchain-test"],
       },
     },
   },
   main: {
     56: {
-      name: 'BSC',
-      chainId: '56',
-      method: 'wallet_addEthereumChain',
+      name: "BSC",
+      chainId: "56",
+      method: "wallet_addEthereumChain",
       chainParmas: {
-        chainId: '0x38',
-        chainName: 'Binance Smart Chain Mainnet',
+        chainId: "0x38",
+        chainName: "Binance Smart Chain Mainnet",
         nativeCurrency: {
-          name: 'BNB',
-          symbol: 'BNB',
+          name: "BNB",
+          symbol: "BNB",
           decimals: 18,
         },
-        rpcUrls: ['https://bsc-dataseed.binance.org'],
-        blockExplorerUrls: ['https://bscscan.com'],
+        rpcUrls: ["https://bsc-dataseed.binance.org"],
+        blockExplorerUrls: ["https://bscscan.com"],
       },
     },
     1: {
-      name: 'ETH',
-      chainId: '1',
-      method: 'wallet_switchEthereumChain',
+      name: "ETH",
+      chainId: "1",
+      method: "wallet_switchEthereumChain",
       chainParmas: {
-        chainId: '0x1',
+        chainId: "0x1",
       },
     },
     66: {
-      name: 'OKEX',
-      chainId: '66',
-      method: 'wallet_addEthereumChain',
+      name: "OKEX",
+      chainId: "66",
+      method: "wallet_addEthereumChain",
       chainParmas: {
-        chainId: '0x42',
-        chainName: 'OKExChain Mainnet',
+        chainId: "0x42",
+        chainName: "OKExChain Mainnet",
         nativeCurrency: {
-          name: 'OKT',
-          symbol: 'OKT',
+          name: "OKT",
+          symbol: "OKT",
           decimals: 18,
         },
-        rpcUrls: ['https://exchainrpc.okex.org'],
-        blockExplorerUrls: ['https://www.oklink.com/okexchain'],
+        rpcUrls: ["https://exchainrpc.okex.org"],
+        blockExplorerUrls: ["https://www.oklink.com/okexchain"],
       },
     },
   },
-}[config.ENV == "dev" ? "test" : "main"]
+}[config.ENV == "dev" ? "test" : "main"];
 
 export const chainList = {
   test: [
     { chainName: "Goerli", chainId: 5, type: "Test" },
-    { chainName: "Ethereum", chainId: 1, type: "Main" }
+    { chainName: "Ethereum", chainId: 1, type: "Main" },
   ],
-  main: [
-    { chainName: "Ethereum", chainId: 1 }
-  ],
-}[config.ENV == "dev" ? "test" : "main"]
-
+  main: [{ chainName: "Ethereum", chainId: 1 }],
+}[config.ENV == "dev" ? "test" : "main"];
 
 /**
  * @description: 链列表
@@ -106,21 +103,19 @@ export const chainList = {
 export const chainData = {
   test: { BSC: 97, ETHEREUM: 5, OKC: 65 },
   main: { BSC: 56, ETHEREUM: 1, OKC: 66 },
-}[config.ENV == "dev" ? "test" : "main"]
+}[config.ENV == "dev" ? "test" : "main"];
 
 /**
  * @description: 链浏览器
  */
 export const chainUrlList = {
   test: [
-    { chainName: "Mumbai", blockExplorerUrls:'https://mumbai.polygonscan.com/' },
-    { chainName: "BSC_TEST", blockExplorerUrls:'https://testnet.bscscan.com/' },
-    { chainName: "OKT_TEST", blockExplorerUrls:'https://www.oklink.com/oktc-test/' }
+    { chainName: "Mumbai", blockExplorerUrls: "https://mumbai.polygonscan.com/" },
+    { chainName: "BSC_TEST", blockExplorerUrls: "https://testnet.bscscan.com/" },
+    { chainName: "OKT_TEST", blockExplorerUrls: "https://www.oklink.com/oktc-test/" },
   ],
-  main: [
-    { chainName: "Ethereum"}
-  ],
-}[config.ENV != "main" ? "test" : "main"]
+  main: [{ chainName: "polygon", blockExplorerUrls: "https://polygonscan.com/tx/" }],
+}[config.ENV != "main" ? "test" : "main"];
 
 /**
  * @description NFT查询
@@ -128,8 +123,8 @@ export const chainUrlList = {
 
 export const getNftAddress = {
   test: "https://testnets-api.opensea.io/api/v1/asset_contract/",
-  main: "https://api.opensea.io/api/v1/asset_contract/"
-}
+  main: "https://api.opensea.io/api/v1/asset_contract/",
+};
 
 /**
  * @description: 代币地址
@@ -138,14 +133,14 @@ export const tokenAddrList = {
   test: {
     BSC: "0xDF074b08910c38D7Cf6550a4A3fF4590080A1c33",
     ETHEREUM: "0x0000000000000000000000000000000000000000",
-    OKC: "0xfA5d51aaBAf744cABEa5cF2F85Be25135c88b054"
+    OKC: "0xfA5d51aaBAf744cABEa5cF2F85Be25135c88b054",
   },
   main: {
     BSC: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
     ETHEREUM: "0x0000000000000000000000000000000000000000",
-    OKC: "0xef71ca2ee68f45b9ad6f72fbdb33d707b872315c"
+    OKC: "0xef71ca2ee68f45b9ad6f72fbdb33d707b872315c",
   },
-}[config.ENV == "dev" ? "test" : "main"]
+}[config.ENV == "dev" ? "test" : "main"];
 
 /**
  * @description: 合约地址
@@ -154,16 +149,14 @@ export const contractList = {
   test: {
     BSC: "0x81be4bfeE7D6c5F1374905B9E4FB37598e52E356",
     ETHEREUM: "0x00ede99e6AaDA1FA874e2c34D0294b8D23e0B869",
-    OKC: "0x5B9582Eb1bb1d76079ef3f652c5aF0D5b35BB636"
+    OKC: "0x5B9582Eb1bb1d76079ef3f652c5aF0D5b35BB636",
   },
   main: {
     BSC: "0xEEB7B5e87249F2eA5bF8A08C36539318243Acd4e",
     ETHEREUM: "0x1dC02fAdD520e73eB2Fbb1589E3d4BE357665b81",
-    OKC: "0x1dC02fAdD520e73eB2Fbb1589E3d4BE357665b81"
+    OKC: "0x1dC02fAdD520e73eB2Fbb1589E3d4BE357665b81",
   },
-}[config.ENV == "dev" ? "test" : "main"]
-
-
+}[config.ENV == "dev" ? "test" : "main"];
 
 //     /**
 //      * @description: 连接钱包
@@ -245,7 +238,6 @@ export const contractList = {
 //           errorHandler(error);
 //           this.fullscreenLoading = false;
 //         });
-
 
 //       return address;
 //     },
